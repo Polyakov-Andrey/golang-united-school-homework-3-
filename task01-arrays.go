11a5a3bfc252895b1ca81fcedcecd80d80a91a57
Andrii Poliakov <Andrii_Poliakov@epam.com>
2022-06-05 10:43:28 +0300

    fix Task-01: clear code

--- task01-arrays.go
+++ task01-arrays.go
@@ -3,13 +3,11 @@ package homework
 func average(input [15]float32) (result float32) {
 	var sum float32 = 0
 	var count int = 0
-	for i, el := range input {
+	for _, el := range input {
 		if el != 0 {
 			count++
 			sum += el
-			println(i, el)
 		}
 	}
-	// println(sum, " ", count)
 	return sum / float32(count)
 }
