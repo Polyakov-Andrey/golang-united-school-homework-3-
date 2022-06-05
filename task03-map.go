6cc0a1344de889de0b27a294c4626510d90a6437
Andrii Poliakov <Andrii_Poliakov@epam.com>
2022-06-05 10:45:04 +0300

    Create tast-02

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,23 @@
 package homework
 
+import (
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+	// fmt.Printf("%+v =>", input)
+	keys := make([]int, len(input))
+
+	i := 0
+	for k := range input {
+		keys[i] = k
+		i++
+	}
+	sort.Ints(keys)
+
+	for _, k := range keys {
+		result = append(result, input[k])
+	}
+	// fmt.Printf("%+v\n", result)
+	return result
 }
